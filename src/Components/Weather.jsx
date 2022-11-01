7fffbe8e3e4d3747bed297eb1c78a647cf8708d4
CristianMld <hkrcrm@gmail.com>
2022-11-01 03:34:05 -0300

    adding loading screen - but i did this after the schedule date time to present the project

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -5,6 +5,7 @@ const Weather = () => {
 
   const [ weather,setWeather ] = useState({});
   const [ isCelsius, setIsCelsius ] = useState(true);
+  const [ isLoading, setIsLoading ] = useState(true);
 
   useEffect(() => {
     
@@ -12,7 +13,10 @@ const Weather = () => {
       const lat = pos.coords.latitude
       const lon = pos.coords.longitude
       axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=118576317ecc4c86d110c56bc71e4732`)
-        .then(res => setWeather(res.data));
+        .then(res => {
+          setIsLoading(false)
+          setWeather(res.data)
+        });
     }
 
     navigator.geolocation.getCurrentPosition(success);
@@ -26,21 +30,28 @@ const Weather = () => {
 
   return (
     <div className='card'>
-      <div className="main-info">
-        <h1>{weather.name}, {weather.sys?.country}.</h1>
-        <h2>{isCelsius ? weatherInCelsius : weatherInFahrenheit} {isCelsius ? '°C' : '°F'}</h2>
-        <img src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}@2x.png`} alt="" />
-        <h2>{weather.weather?.[0].description}</h2>
-      </div>
-
-      <div className="additional-info">
-        <h3><i className="fa-solid fa-wind"></i> wind speed: {Math.round(weather.wind?.speed * 3.6)} km/h</h3>
-        <h3><i className="fa-solid fa-droplet"></i> humidity: {weather.main?.humidity}%</h3>
-        <h3><i className="fa-solid fa-temperature-half"></i> thermal sensation: {' '} 
-        {isCelsius ? thermalSensation : sensationInFahrenheit} {isCelsius ? '°C' : '°F'}</h3>
-        <button onClick={() => setIsCelsius(!isCelsius)}>
-        {isCelsius ? 'switch to Fahrenheit' : 'switch to Celsius'}</button>
-      </div>
+      {
+        isLoading ? (
+          <h1>Loading...</h1>
+        ) : (
+          <>
+            <div className="main-info">
+              <h1>{weather.name}, {weather.sys?.country}.</h1>
+              <h2>{isCelsius ? weatherInCelsius : weatherInFahrenheit} {isCelsius ? '°C' : '°F'}</h2>
+              <img src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}@2x.png`} alt="" />
+              <h2>{weather.weather?.[0].description}</h2>
+            </div>
+            <div className="additional-info">
+              <h3><i className="fa-solid fa-wind"></i> wind speed: {Math.round(weather.wind?.speed * 3.6)} km/h</h3>
+              <h3><i className="fa-solid fa-droplet"></i> humidity: {weather.main?.humidity}%</h3>
+              <h3><i className="fa-solid fa-temperature-half"></i> thermal sensation: {' '} 
+              {isCelsius ? thermalSensation : sensationInFahrenheit} {isCelsius ? '°C' : '°F'}</h3>
+              <button onClick={() => setIsCelsius(!isCelsius)}>
+              {isCelsius ? 'switch to Fahrenheit' : 'switch to Celsius'}</button>
+            </div>
+          </>
+        )
+      }      
     </div>
   );
 };
